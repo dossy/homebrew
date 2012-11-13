df597cad9994789eee1f62a29265b395a00db694
Dossy Shiobara <dossy@panoptic.com>
2012-11-12 23:17:29 -0500

    Add optional Lua support.

--- Library/Formula/htt.rb
+++ Library/Formula/htt.rb
@@ -6,13 +6,22 @@ class Htt < Formula
   sha1 'fee2ef8a3ffd3ac9fd6b4d3378dc484c19ae5e94'
   version '2.2.11'
 
+  option 'with-lua', "Compile with Lua support"
+
   depends_on 'pcre'
 #* libapr-1
 #* libapr-util-1
 #* libssl
+  depends_on 'lua' if build.include? 'with-lua'
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    args = %W[
+      --prefix=#{prefix}
+    ]
+
+    args << "--enable-lua-module" if build.include? 'with-lua'
+
+    system "./configure", *args
     system "make install"
   end
 end
